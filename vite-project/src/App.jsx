7d445af02e2a22ad023c64555e8321295d5d455f
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2024-11-24 22:36:14 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,98 +1,99 @@
-import { useCallback, useState, useEffect, useRef } from "react";
-import "./App.css";
+import { useCallback, useState, useEffect, useRef} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  const [length, setLength]= useState(8);
+  const [ numberAllowed,  setNumberAllowed]= useState(false);
+  const [charAllowed, setCharAllowed]= useState(false);
+  const [passsword, setPassword]= useState("");
 
-  // useRef hook
+  //useRef hook
+
   const passwordRef = useRef(null);
 
-  const passwordGenerator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (numberAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$^&*-_+=[]{}`~";
 
-    for (let i = 1; i < length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
+  const passwordGenerator = useCallback(()=>{  
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$^&*-_+=[]{}~"
+
+    for(let i =1;i<length;i++){
+      let char = Math.floor(Math.random()* str.length+1)
+      pass += str.charAt(char)
+
     }
-    setPassword(pass);
-  }, [length, numberAllowed, charAllowed]);
+    setPassword(pass)
+    
+  }, [length, numberAllowed, charAllowed])
 
-  const copyPasswordToClipboard = useCallback(() => {
+  const copyPasswordToClipboard = useCallback(()=>{
     passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 101);
-    window.navigator.clipboard.writeText(password);
-  }, [password]);
+    passwordRef.current?.setSelectionRange(0,101);
+    window.navigator.clipboard.writeText(passsword)
+  },[passsword])
 
-  useEffect(() => {
+
+  useEffect(()=>{
     passwordGenerator();
-  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  },[length, numberAllowed, charAllowed,passwordGenerator]);
 
+    
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-800 text-orange-500">
-      <div className="w-full max-w-md shadow-md rounded-lg px-4 py-6 bg-gray-700">
-        <h1 className="text-white text-center my-3 text-xl">Password Generator</h1>
-        <div className="flex shadow-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-2 px-3 text-gray-900"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 bg-gray-700 text-orange-500 '
+      >
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='className="flex shadow-lg overflow-hidden mb-4'>
+          <input type="text"
+          value={passsword}
+          className='outline-none w-full pay-1 px-3'
+          placeholder='Password'
+          readOnly 
+          ref={passwordRef}/>
+          <button onClick={copyPasswordToClipboard} className='outlinr-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          >copy</button>
+        
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type='range'
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}/>
+          <label>Length: {length}</label>
+
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={()=>{
+            setNumberAllowed((prev)=>!prev);
+          }} 
           />
-          <button
-            onClick={copyPasswordToClipboard}
-            className="outline-none bg-blue-700 text-white px-3 py-1 shrink-0"
-          >
-            Copy
-          </button>
+          <label htmlFor='numberInput'>Numbers</label>
         </div>
-        <div className="flex flex-col gap-y-4 text-sm">
-          <div className="flex items-center justify-between">
-            <label>Length: {length}</label>
-            <input
-              type="range"
-              min={6}
-              max={100}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setLength(e.target.value);
-              }}
-            />
-          </div>
-          <div className="flex items-center gap-x-2">
-            <input
-              type="checkbox"
-              defaultChecked={numberAllowed}
-              id="numberInput"
-              onChange={() => {
-                setNumberAllowed((prev) => !prev);
-              }}
-            />
-            <label htmlFor="numberInput">Numbers</label>
-          </div>
-          <div className="flex items-center gap-x-2">
-            <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="characterInput"
-              onChange={() => {
-                setCharAllowed((prev) => !prev);
-              }}
-            />
-            <label htmlFor="characterInput">Characters</label>
-          </div>
-        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={charAllowed}
+          id="characterInput"
+          onChange={()=>{
+            setNumberAllowed((prev)=>!prev);
+          }} 
+          />
+          <label htmlFor='characterInput'>Characters</label>
+      </div>
+      
       </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+
+ export default App  
